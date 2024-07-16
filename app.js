9ea4735c3c4ef0dd487dd800735ae2063ab04b2e
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2024-07-16 11:49:58 +0500

    02

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
-const cars = [
-    {
-        'type': 'Fiat',
-        'model': '500',
-        'color': 'white',
-        'info': function() { return `${this.type} ${this.model} ${this.color}`; }
-    },
-    {
-        'type': 'Porsche',
-        'model': '911',
-        'color': 'yellow',
-        'info': function() { return `${this.type} ${this.model} ${this.color}`; }
-    },
-    {
-        'type': 'BMW M5',
-        'model': 'CS',
-        'color': 'black',
-        'info': function() { return `${this.type} ${this.model} ${this.color}`; }
-    },
-];
+
+let car = {
+    'type': 'Fiat',
+    'model': '500',
+    'color': 'white',
+    'info': function () { return `${this.type} ${this.model} ${this.color}`; }
+}
 
 
-cars.forEach(car => {
-    console.log(car.info());
-})
+for (const key in car) {
+    let value = car[key];
+}
+
+
+for (const [key, value] of Object.entries(car)) {
+    console.log(key, value);
+}
+
